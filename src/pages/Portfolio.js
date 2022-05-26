bdb177591ac3787a862901b98e169c8499bf78ec
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-05-25 17:12:10 -0700

    project description update

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -16,9 +16,11 @@ export default function Portfolio() {
                 <a href="https://github.com/kingnolds/Browser-Party">https://github.com/kingnolds/Browser-Party</a> <br></br>
                 <a href="https://browser-party.herokuapp.com">https://browser-party.herokuapp.com</a> <br></br>
                 <br></br>
-                Online application that allows users to create or join a lobby and compete with other players in up to 3 mini games. Created dynamic pages by routing components and passing props with React.js, using CSS animations, and using stylized colors and images. Created game lobby chat with Socket.IO. Languages and tools: React.js, Socket.IO, Adobe Photoshop
-            </p>    
-            <br></br>           
+                This is an online application that allows users to create or join lobbies, chat with other players, and compete against them in up to 3 mini games. My work for this project consisted of styling the interface and making it mobile responsive, connecting React components, and confirming that backend information was rendering on the client side.
+                <br></br>
+                <br></br> 
+                Languages and tools: React.js, Socket.IO, Adobe Photoshop
+            </p>           
         </li>
             <li>
                 <h3>Ponder</h3>
@@ -29,9 +31,11 @@ export default function Portfolio() {
                     <a href="https://github.com/Chrisle206/ponder">https://github.com/Chrisle206/ponder</a> <br></br>
                     <a href="https://pacific-ravine-12135.herokuapp.com">https://pacific-ravine-12135.herokuapp.com</a> <br></br>
                     <br></br>
-                    Social media platform that allows users to post anonymously, upvote, downvote, and respond to posts. Created templates to render 3 most recent posts. Used JavaScript to connect modular routes to front end components and MongoDB to link users to posts. Languages and tools: Node, Handlebars, Bootstrap, MongoDB
+                    Ponder is a social media platform that allows users to post anonymously, upvote, downvote, and respond to posts. I worked with Handlebars to create templates to refactor our workflow. Using JavaScript, I connected modular routes to front end components and created the upvote/downvote limit.
+                    <br></br> 
+                    <br></br> 
+                    Languages and tools: Node, Handlebars, Bootstrap, MongoDB
                 </p>
-                <br></br>
             </li>
             <li>
                 <h3>Furiendr</h3>
@@ -42,7 +46,10 @@ export default function Portfolio() {
                     <a href="https://github.com/mweyer/First-Group-Project">https://github.com/mweyer/First-Group-Project</a> <br></br>
                     <a href="https://mweyer.github.io/First-Group-Project">https://mweyer.github.io/First-Group-Project</a> <br></br>
                     <br></br>
-                    Pet adoption website using Rescue API to search locally for rescue cats and dogs. Studied API documentation to apply filters to fetch requests to allow attributes for searches. Used jQuery to obtain input field values to pass into backend requests. Languages and tools: HTML5, CSS, Javascript, jQuery, MaterializeUI
+                    This is a rescue pet adoption website using RescueGroup.org's API to search locally for pets. I studied API documentation to apply filters to fetch requests to allow attributes for searches. Using jQuery, I was able to obtain input values and pass them into backend requests. 
+                    <br></br> 
+                    <br></br> 
+                    Languages and tools: HTML5, CSS, Javascript, jQuery, MaterializeUI
                 </p>
             </li>
         </ul>
