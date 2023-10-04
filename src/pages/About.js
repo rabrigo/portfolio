f64aeaff9bcaab16e132f6024d1e3ba57a3a5f23
the rawness <therawness@thes-Mac-mini.local>
2023-10-03 18:41:00 -0700

    revised about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
                 <img className="laptop-pic" src={Laptop}/>
             </div>
             <p className="about-text"> 
-            My name is Ronnel and I am a Software Developer. This past March, I received my certificate in Full Stack Development. This page contains my group projects from this program. Some of the technologies I enjoyed delving into include: JavaScript, HTML, CSS, Node, and React. I discovered my knack for developing smooth functionality and interfaces. You can see it throughout my apps on the project page. Outside of programming, I enjoy making music, filming and editing videos, and dancing.
+            My name is Ronnel and I am a Software Developer. In March of 2022, I received my certificate in Full Stack Development. This page contains my group projects from the coding boot camp I completed. Some of the technologies I enjoyed delving into include: JavaScript, HTML, CSS, Node, and React. During this program, I discovered my knack for developing smooth functionality and interfaces. You can see it throughout my apps on the project page. Outside of programming, I enjoy making music, videography, and dancing.
             </p>
         </div>
     )
